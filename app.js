8b276aa6cd6f024bef21dd7f3678ce551b84bff9
sanjay128yadav <sanjay128yadav@gmail.com>
2025-09-14 16:03:27 +0530

    CI CD

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello World CI CD!',
+        message: 'Hello World CI CD1!',
         timestamp: new Date().toISOString(),
         environment: process.env.NODE_ENV || 'development'
     });
